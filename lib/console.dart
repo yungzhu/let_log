1746cc43786a09e58db00e95fe3647ba16817a2b
yung <imyung@163.com>
2020-06-15 21:17:28 +0800

    git init

---
+++ lib/console.dart
@@ -0,0 +1,10 @@
+class Console {
+  static void log(Object value) {}
+  static void warn(Object value) {}
+  static void error(Object value) {}
+  static void time(Object value) {}
+  static void endTime(Object value) {}
+  static void clear() {}
+  static void net(String api) {}
+  static void endNet(String api, {int status}) {}
+}
---
+++ lib/net_log.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class NetLog {
+  static void start({
+    @required String api,
+  }) {}
+  static void end(int stateCode) {}
+}
